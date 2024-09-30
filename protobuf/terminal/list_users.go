0892d35c94e1d3431e95b064d9563d07c5851e1d
adkuldoshin <adkuldoshin@avito.ru>
2024-09-30 20:56:04 +0300

    fix id

--- protobuf/terminal/list_users.go
+++ protobuf/terminal/list_users.go
@@ -7,26 +7,32 @@ import (
 
 var users = map[string]*pb.User{
 	"25bdd851-631b-4cff-a8c2-0d090b485bed": &pb.User{
+		Id:        "25bdd851-631b-4cff-a8c2-0d090b485bed",
 		FirstName: "Alexey",
 		LastName:  "Kuldoshin",
 	},
 	"7c659e53-9871-4297-9084-d1ce251af68d": &pb.User{
+		Id:        "7c659e53-9871-4297-9084-d1ce251af68d",
 		FirstName: "Andrey",
 		LastName:  "Orel",
 	},
 	"e8906f30-c13b-457f-a142-dac4fd429656": &pb.User{
+		Id:        "e8906f30-c13b-457f-a142-dac4fd429656",
 		FirstName: "Ildar",
 		LastName:  "Lutfullin",
 	},
 	"784fc6d5-5fdd-4b90-baee-71df6caa4c59": &pb.User{
+		Id:        "784fc6d5-5fdd-4b90-baee-71df6caa4c59",
 		FirstName: "Evgeny",
 		LastName:  "Tushkanov",
 	},
 	"f833ba18-5e01-449c-a98b-ff5602f4c812": &pb.User{
+		Id:        "f833ba18-5e01-449c-a98b-ff5602f4c812",
 		FirstName: "Nadezhda",
 		LastName:  "Filippova",
 	},
 	"73878c88-a90d-451d-bfe5-2572acc8f5f6": &pb.User{
+		Id:        "73878c88-a90d-451d-bfe5-2572acc8f5f6",
 		FirstName: "Polina",
 		LastName:  "Romanchenko",
 	},
@@ -47,8 +53,8 @@ func (h *Handler) ListUsers(ctx context.Context, in *pb.ListUsersIn) (*pb.ListUs
 			}, nil
 		case "784fc6d5-5fdd-4b90-baee-71df6caa4c59":
 			return &pb.ListUsersOut{
-			UserIds: []string{"e8906f30-c13b-457f-a142-dac4fd429656", "73878c88-a90d-451d-bfe5-2572acc8f5f6"},
-		}, nil
+				UserIds: []string{"e8906f30-c13b-457f-a142-dac4fd429656", "73878c88-a90d-451d-bfe5-2572acc8f5f6"},
+			}, nil
 		default:
 			return nil, nil
 		}
