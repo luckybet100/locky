c9c0f7c7d8ea0f84a770a2371e68484296d5eded
adkuldoshin <adkuldoshin@avito.ru>
2024-09-30 21:06:57 +0300

    fix

--- protobuf/terminal/set_user_face.go
+++ protobuf/terminal/set_user_face.go
@@ -3,9 +3,11 @@ package terminal
 import (
 	"context"
 	pb "github.com/Locky-Inc/locky/internal/generated/protobuf/terminal"
+	"log"
 )
 
 func (h *Handler) SetUserFace(ctx context.Context, in *pb.SetUserFaceIn) (*pb.SetUserFaceOut, error) {
+	log.Println("setUserFace", in)
 	if _, ok := users[in.UserId]; ok {
 		users[in.UserId].Face = in.Face
 		return &pb.SetUserFaceOut{User: users[in.UserId]}, nil
